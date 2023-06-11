6440b59d51faf55ddb574ffcaf9cafc3da07ca30
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-11 03:23:38 +0300

    🐛 fix(error.middleware.ts): use Default constants for error message and status code

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,15 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@/exceptions/httpException';
 import { logger } from '@utils/logger';
+import { Default } from '@constants';
 
 export const ErrorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
-  try {
-    const status: number = error.status || 500;
-    const message: string = error.message || 'Something went wrong';
+    try {
+        const status: number = error.status || Default.ERROR_STATUS_CODE;
+        const message: string = error.message || Default.ERROR_MESSAGE;
 
-    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
-    res.status(status).json({ message });
-  } catch (error) {
-    next(error);
-  }
+        logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
+        res.status(status).json({ message });
+    } catch (error) {
+        next(error);
+    }
 };
