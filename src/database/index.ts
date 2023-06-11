76c3912763e72991ed2528ee4fe740f68cbf44b7
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-11 03:20:12 +0300

    🐛 fix(database/index.ts): change sequelize connection string to use postgres dialect and ssl connection, add Messages model

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,34 +1,35 @@
 import Sequelize from 'sequelize';
 import { NODE_ENV, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from '@config';
 import UserModel from '@models/users.model';
+import MessageModel from '@models/messages.model';
 import { logger } from '@utils/logger';
+import { Env } from '@constants';
 
-const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-  dialect: 'mysql',
-  host: DB_HOST,
-  port: DB_PORT,
-  timezone: '+09:00',
-  define: {
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_general_ci',
-    underscored: true,
-    freezeTableName: true,
-  },
-  pool: {
-    min: 0,
-    max: 5,
-  },
-  logQueryParameters: NODE_ENV === 'development',
-  logging: (query, time) => {
-    logger.info(time + 'ms' + ' ' + query);
-  },
-  benchmark: true,
+const sequelize = new Sequelize.Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}?ssl=true`, {
+    dialect: 'postgres',
+    port: +DB_PORT,
+    pool: {
+        min: 0,
+        max: 5
+    },
+    logQueryParameters: NODE_ENV === Env.Development,
+    logging: (query, time) => {
+        logger.info(`${time}ms ${query}`);
+    },
+    benchmark: true
 });
 
-sequelize.authenticate();
+sequelize.authenticate()
+    .then(() => {
+        logger.info('The database is connected.');
+    })
+    .catch((error: Error) => {
+        logger.error(`Unable to connect to the database: ${error}.`);
+    });
 
 export const DB = {
-  Users: UserModel(sequelize),
-  sequelize, // connection instance (RAW queries)
-  Sequelize, // library
+    Users: UserModel(sequelize),
+    Messages: MessageModel(sequelize),
+    sequelize,
+    Sequelize
 };
